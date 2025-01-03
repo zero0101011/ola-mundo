2457834f043b0b30d34b5fe61e5768c360644c84
Samuel Carlos <102079472+zero0101011@users.noreply.github.com>
2025-01-03 20:51:04 -0300

    react markdown

--- src/Componentes/PostModelo/index.js
+++ src/Componentes/PostModelo/index.js
@@ -20,7 +20,7 @@ export default function PostModelo({ fotoCapa, titulo, children }) {
 
             </div>
 
-
+            
 
         </article>
     )
--- src/paginas/Post/index.js
+++ src/paginas/Post/index.js
@@ -1,10 +1,34 @@
 import { useParams } from "react-router-dom";
+import posts from "json/posts.json";
+import PostModelo from "Componentes/PostModelo";
+import ReactMarkdown from "react-markdown"
+import './Post.css'
+
 
 export default function Post () {
-    
+
     const parametros = useParams();
+
+    const post = posts.find(post => post.id === Number(parametros.id));
+    
+
+
+    console.log(post)
     
     return (
-        <h1>Post {parametros.id} </h1>
+        <PostModelo 
+
+        fotoCapa={`/assets/posts/${post.id}/capa.png`} 
+        titulo={post.titulo}>
+
+
+        <div className="post-markdown-container" >
+        <ReactMarkdown>{post.texto}</ReactMarkdown>
+        </div>
+        
+     
+          
+
+        </PostModelo>
     )
 }
